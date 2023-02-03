e3ba5b483455f265b9c7816785aaca272c4918c6
Tarleshsin <tarlesh_parmar@yahoo.com>
2023-02-03 14:34:02 -0500

    Single file commit

---
+++ src/main/java/ScreenShot/Screenshot_CHATGPT.java
@@ -0,0 +1,44 @@
+package ScreenShot;
+
+
+
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Screenshot_CHATGPT {
+  
+	public static WebDriver driver;
+	public static void main(String[] args) {
+	  
+	  WebDriverManager.chromedriver().setup();
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
+	  driver.get("https://www.amazon.com");
+	  
+    try {
+      Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+      Rectangle screenRectangle = new Rectangle(screenSize);
+      Robot robot = new Robot();
+      BufferedImage image = robot.createScreenCapture(screenRectangle);
+      ImageIO.write(image, "png", new File("screenshots/ChatGpt.png"));
+    } catch (Exception e) {
+      e.printStackTrace();
+      
+      driver.quit();
+    }
+  }
+}
+
+
+
